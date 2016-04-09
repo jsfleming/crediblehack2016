4faba80cc359e493f86882e8e6a2978aee11aca3
jsfleming <josofl12@gmail.com>
2016-04-09 16:55:47 -0400

    working on setting up the database for clients

--- credible/clients/models.py
+++ credible/clients/models.py
@@ -3,3 +3,21 @@ from __future__ import unicode_literals
 from django.db import models
 
 # Create your models here.
+# field types: BooleanField, CharField(max_length),
+# IntegerField(default), DateField
+
+class Profile(models.Model):
+    
+
+class Allergies(models.Model):
+    
+
+
+class Contacts(models.Model):
+
+
+class Medications(models.Model):
+
+
+class Diagnoses(models.Model):
+
