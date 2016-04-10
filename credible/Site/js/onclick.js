26023986f86779097ca680cda85b7423ede59725
TERRY <taekim97@gmail.com>
2016-04-10 09:44:14 -0400

    minor fixes

--- credible/Site/js/onclick.js
+++ credible/Site/js/onclick.js
@@ -1,4 +1,17 @@
+$( document ).ready(function() {
+   	
+    $("#password").keyup(function(event){
+	    if(event.keyCode == 13){
+	        $("#login").click();
+	    }
+	});
+});
+
+
 function clickButton() {
+	document.location.href = "template-main.html"
+
+	/*
 	var e_id=$("#username").val();
 	var pass=$("#password").val();
 	$.post("https://stageservices.crediblebh.com/bitcamp/api/account/mobilelogin",
@@ -6,16 +19,15 @@ function clickButton() {
 		function checkRequest(data, status) {
 			//console.log("Status: " + status)
 			//console.log("Data: " + JSON.stringify(data))
-			if (data.LoginStatus != 1) {
-				alert("Invalid Username and/or Password")
-			} else {
-				var myCookie = "ID=" + data.EmployeeId + "; expires=0; path=/"
-				console.log(myCookie)
-				document.cookie = myCookie;
-				console.log("myCookie: " + document.cookie)
+			
+			//if (data.LoginStatus != 1) {
+				//alert("Invalid Username and/or Password")
+			//} else {
+				//var myCookie = "ID=" + data.EmployeeId + "; expires=0; path=/"
+				//document.cookie = myCookie;
 				document.location.href = "template-main.html"
 			}
 		},
-		"json")
+		"json")*/
 }
 
