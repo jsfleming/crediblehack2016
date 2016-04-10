f04795ef399c972cc5e05fd9e1af56bc48e26e03
jsfleming <josofl12@gmail.com>
2016-04-10 07:46:11 -0400

    changed a lot of stuff

--- credible/employees/views.py
+++ credible/employees/views.py
@@ -2,19 +2,26 @@ from django.shortcuts import render
 from django.http import HttpResponse
 from django.template import loader
 from django.template import RequestContext
+import datetime
+import requests
+import json
+from gettext import lngettext
 
 import json
 from .models import Profile
+from .models import Schedules
+import Generate_Link
 
 # Create your views here.
 
 def index(request):
-#    employee_address_list = Profile.objects.get(pk=1)
+
     template = loader.get_template('template-main.html')
 
 
-    data = json.dumps({'a': 234234})
-    c = RequestContext(request, {'foo': 234234})
+    data = Generate_Link.get_link()
+    data2 = json.dumps({'a': 234234})
+    c = RequestContext(request, {'map_link': data})
 
 #    return HttpResponse(request, 'employees/main.html', context)
     return HttpResponse(template.render(c))
