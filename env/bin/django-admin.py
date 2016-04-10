328363b19bc5446cf3cfa70e859d18fcebffd8f5
jsfleming <josofl12@gmail.com>
2016-04-09 21:02:29 -0400

    install django to virtualenv, messed with databasing

---
+++ env/bin/django-admin.py
@@ -0,0 +1,5 @@
+#!/Users/jfleming/Documents/Programming/Hackathons/Bitcamp/2016/crediblehack2016/env/bin/python
+from django.core import management
+
+if __name__ == "__main__":
+    management.execute_from_command_line()
